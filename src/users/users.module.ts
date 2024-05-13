415837419d07d763726d25a205cdf1f670badc53
kamil peschke <kamil.peschke@gmail.com>
2024-05-13 16:51:43 +0200

    Adding middlewares

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,20 @@
-import { Module } from '@nestjs/common';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { Module, MiddlewareConsumer} from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import {User} from './users.entity';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/user.interceptor';
+import { CurrentUserMiddleware } from './middlewares/current.user.middleware';
 
 
 @Module({
   imports:[TypeOrmModule.forFeature([User])], //create a repository 
   controllers: [UsersController],
-  providers: [UsersService, AuthService, 
-    {
-    provide: APP_INTERCEPTOR,
-    useClass: CurrentUserInterceptor
-  }],
+  providers: [UsersService, AuthService,],
 })
-export class UsersModule {}
+export class UsersModule {
+
+  configure(consumer:MiddlewareConsumer){
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}
